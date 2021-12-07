7759e443695cf2df03f77d9be43ac3236ce8b134
Tomlacko <72975448+Tomlacko@users.noreply.github.com>
2021-12-07 14:15:39 +0100

    cooldown & comments

--- cogs/andrew_thing.py
+++ cogs/andrew_thing.py
@@ -19,6 +19,7 @@ class AndrewThing(commands.Cog):
         self.unsent_messages = 0
         self.last_channel = None
         self.timer_task = None
+        self.last_triggered_when = None
 
 
     @commands.Cog.listener()
@@ -26,15 +27,23 @@ class AndrewThing(commands.Cog):
         if user.id != andrewID:
             return
         
-        if not self.last_typing_when or self.last_typing_when < when-timedelta(minutes=30):
+        #cooldown to prevent triggering itself repeatedly
+        if self.last_triggered_when and self.last_triggered_when > when-timedelta(hours=2):
+            return
+
+        #reset progress if no further typing was done for a while
+        if self.last_typing_when and self.last_typing_when < when-timedelta(minutes=20):
             self.typing_attempts = 0
             self.unsent_messages = 0
 
+        #update values with new ones
         self.last_channel = channel
         self.last_typing_when = when
         self.typing_attempts+=1
 
+        #do not wait for the typing to stop if it's been going on for a long time already
         if self.typing_attempts < 5:
+            #keep trying to wait and see when the typing stops
             if self.timer_task:
                 self.timer_task.cancel()
             self.timer_task = asyncio.create_task(self.countdown())
@@ -42,6 +51,7 @@ class AndrewThing(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, msg: discord.Message):
         if msg.author.id == andrewID:
+            #reset all progress if andrew actually ends up sending a message
             if self.timer_task:
                 self.timer_task.cancel()
             self.typing_attempts = 0
@@ -53,16 +63,22 @@ class AndrewThing(commands.Cog):
 
     
     async def trySayTheThing(self):
+        #if this function triggers, andrew has stopped typing for a while
+
         self.typing_attempts = 0
         self.unsent_messages+=1
 
+        #don't trigger the first few times andrew stops tying
         if self.unsent_messages < 3:
             return
         
+        #the more andrew "does the thing", the higher the chance of triggering the message
         if random.randint(1, self.unsent_messages) < 3:
             return
         
+        #all checks passed, send the thing
         self.unsent_messages = 0
+        self.last_triggered_when = datetime.now()
         await self.last_channel.send("Uh oh.. Andrew is doing *the thing* again...")
 
 
